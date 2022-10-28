4a9414ab8c7e2442a7338872e6244821bf76fa26
yatsumehole <yatsumehole@naver.com>
2022-10-28 16:01:07 +0000

    localStorage 관련 글자 설정 추가

--- js/global.js
+++ js/global.js
@@ -1,9 +1,13 @@
+const CHAR_SETTING_NAME = "MSM_CHAR_TABLE";
+
 function $(selector) {
   const elemArray = document.querySelectorAll(selector);
   const length = elemArray.length;
   if (length == 1) {
     const elem = document.querySelector(selector);
     return elem;
+  } else if (length == 0) {
+    return null;
   } else {
     return elemArray;
   }
