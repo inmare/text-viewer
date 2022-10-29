f31e1ee1de86c04546d97253efc27150273b1205
yatsumehole <yatsumehole@naver.com>
2022-10-29 14:15:20 +0000

    키보드 단축키 설정 추가

--- js/util/shortcut.js
+++ js/util/shortcut.js
@@ -4,10 +4,42 @@ class Shortcut {
   }
 
   static detectShortcut(e) {
-    if (e.code == "KeyV" && e.altKey) {
-      const changeViewBtn = $("#change-view");
-      const event = new Event("click");
-      changeViewBtn.dispatchEvent(event);
+    const isTextTableActivate = checkTextViewActivate();
+    if (isTextTableActivate) {
+      for (let [_, value] of Object.entries(TEXT_TABLE_SHORTCUT)) {
+        const isSubKeyMatch = checkSubKeyMatch(value);
+        if (value.key == e.key && isSubKeyMatch) {
+          // do something about text table
+          TextView.moveSelPos(e.key);
+          return;
+        }
+      }
+    }
+
+    for (let [_, value] of Object.entries(SHORTCUT)) {
+      const isSubKeyMatch = checkSubKeyMatch(value);
+      if (value.key == e.key && isSubKeyMatch) {
+        // do something about shortcut
+        return;
+      }
+    }
+
+    function checkTextViewActivate() {
+      const selectTd = $(".select");
+      if (selectTd) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function checkSubKeyMatch(shortcut) {
+      const isSubKeyMatch =
+        shortcut.altKey == e.altKey &&
+        shortcut.ctrlKey == e.ctrlKey &&
+        shortcut.shiftKey == e.shiftKey;
+
+      return isSubKeyMatch;
     }
   }
 }
