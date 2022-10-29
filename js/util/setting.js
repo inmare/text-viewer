51088acdba54421e321cb94c773e9980ea740cec
yatsumehole <yatsumehole@naver.com>
2022-10-29 14:15:08 +0000

    키보드 설정 추가

--- js/util/setting.js
+++ js/util/setting.js
@@ -4,7 +4,55 @@ const DATABASE_NAME = "MSM_DATABASE";
 const OBJECT_STORE_NAME = "MSM_OBJECT_STORE";
 const ID_HASH_LENGTH = 4;
 
-let charList = {
+const charList = {
   from: [" ", "0", "8", "5", "$", "`", "~", "Q", "D"],
   to: ["Γ", "Δ", "δ", "Σ", "§", "Я", "Ξ", "¶", "Ю"],
+};
+
+const TEXT_TABLE_SHORTCUT = {
+  moveRight: {
+    key: "ArrowRight",
+    altKey: false,
+    ctrlkey: false,
+    shiftKey: false,
+  },
+  moveLeft: {
+    key: "ArrowLeft",
+    altKey: false,
+    ctrlkey: false,
+    shiftKey: false,
+  },
+  moveUp: {
+    key: "ArrowUp",
+    altKey: false,
+    ctrlkey: false,
+    shiftKey: false,
+  },
+  moveDown: {
+    key: "ArrowDown",
+    altKey: false,
+    ctrlkey: false,
+    shiftKey: false,
+  },
+  putText: {
+    key: /^([\u0020-\u007e]|Enter|Tab)$/,
+    altKey: false,
+    ctrlkey: false,
+    shiftKey: false,
+  },
+};
+
+const SHORTCUT = {
+  save: {
+    key: "s",
+    altKey: false,
+    ctrlkey: true,
+    shiftKey: false,
+  },
+  changeView: {
+    key: "v",
+    altKey: true,
+    ctrlkey: false,
+    shiftKey: false,
+  },
 };
