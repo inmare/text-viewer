34fdcc012a320743924ba191fa381a07e563b5a0
yatsumehole <yatsumehole@naver.com>
2022-10-29 07:58:09 +0000

    파일 추가에 따른 초기화 코드 추가

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   window.$ = $;
+  Database.initialize();
   CharTable.initialize();
   TextView.initialize();
   Header.initialize();
   Shortcut.initialize();
+  DataView.initialize();
 });
