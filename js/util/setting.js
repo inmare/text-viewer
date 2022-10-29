ba3d78e01526a9e9c409bf45162ca2fb96f7c2e8
yatsumehole <yatsumehole@naver.com>
2022-10-29 07:57:17 +0000

    데이터 베이스 관련 설정 추가

--- js/util/setting.js
+++ js/util/setting.js
@@ -1,6 +1,8 @@
 // 설정들 앞에는 접두사 MSM(만사마)가 붙음
 const CHAR_SETTING_NAME = "MSM_CHAR_TABLE";
 const DATABASE_NAME = "MSM_DATABASE";
+const OBJECT_STORE_NAME = "MSM_OBJECT_STORE";
+const ID_HASH_LENGTH = 4;
 
 let charList = {
   from: [" ", "0", "8", "5", "$", "`", "~", "Q", "D"],
