de650846196cc615e882e824b714ba37d0654721
yatsumehole <yatsumehole@naver.com>
2022-10-29 11:12:11 +0000

    데이터베이스에서 로드한 이미지를 보여주는 함수 추가

--- js/ui/imageView.js
+++ js/ui/imageView.js
@@ -1,17 +1,20 @@
 class ImageView {
-  static initialize() {
-    // 임시로 이미지 그리기
-    const img = new Image();
-    img.src = "./lorem-short.png";
-    img.onload = function () {
-      const imageCanvas = $("#image-canvas");
-      imageCanvas.width = img.width;
-      imageCanvas.height = img.height;
-      const ctx = imageCanvas.getContext("2d");
-      ctx.drawImage(img, 0, 0);
-      const rectCanvas = $("#rect-canvas");
-      rectCanvas.width = img.width;
-      rectCanvas.height = img.height;
-    };
+  static initialize() {}
+
+  static updateImageView(info) {
+    const imageCanvas = $("#image-canvas");
+    imageCanvas.width = info.size[0];
+    imageCanvas.height = info.size[1];
+    imageCanvas.style.width = info.size[0] + "px";
+    imageCanvas.style.height = info.size[1] + "px";
+
+    const scaleFactor = 300 / 96; // 300dpi로 설정
+    imageCanvas.width = Math.ceil(imageCanvas.width * scaleFactor);
+    imageCanvas.height = Math.ceil(imageCanvas.height * scaleFactor);
+
+    const ctx = imageCanvas.getContext("2d");
+    ctx.globalCompositeOperation = "luminosity";
+    ctx.scale(scaleFactor, scaleFactor);
+    ctx.putImageData(info.data, 0, 0);
   }
 }
