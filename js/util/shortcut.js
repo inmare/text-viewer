ce50458cb672bb1f39e56475ab644378481b48a6
yatsumehole <yatsumehole@naver.com>
2022-10-29 14:34:08 +0000

    단축키 변수를 Shortcut 클래스 안으로 이동

--- js/util/shortcut.js
+++ js/util/shortcut.js
@@ -42,4 +42,61 @@ class Shortcut {
       return isSubKeyMatch;
     }
   }
+
+  // 모듈을 사용할 수 없기 때문에 다른 함수들이 다 선언되고 난 뒤인
+  // Shortcut 클래스 안에 직접 선언을 해서 단축키에 callback을 설정 할 수 있도록 함
+  static TEXT_TABLE_SHORTCUT = {
+    moveRight: {
+      key: "ArrowRight",
+      altKey: false,
+      ctrlkey: false,
+      shiftKey: false,
+      callback: TextView.moveTdPos,
+    },
+    moveLeft: {
+      key: "ArrowLeft",
+      altKey: false,
+      ctrlkey: false,
+      shiftKey: false,
+      callback: TextView.moveTdPos,
+    },
+    moveUp: {
+      key: "ArrowUp",
+      altKey: false,
+      ctrlkey: false,
+      shiftKey: false,
+      callback: TextView.moveTdPos,
+    },
+    moveDown: {
+      key: "ArrowDown",
+      altKey: false,
+      ctrlkey: false,
+      shiftKey: false,
+      callback: TextView.moveTdPos,
+    },
+    putText: {
+      key: /^([\u0020-\u007e]|Enter|Tab)$/,
+      altKey: false,
+      ctrlkey: false,
+      shiftKey: false,
+      callback: TextView.changeTdText,
+    },
+  };
+
+  static SHORTCUT = {
+    save: {
+      key: "s",
+      altKey: false,
+      ctrlkey: true,
+      shiftKey: false,
+      callback: null,
+    },
+    changeView: {
+      key: "v",
+      altKey: true,
+      ctrlkey: false,
+      shiftKey: false,
+      callback: null,
+    },
+  };
 }
