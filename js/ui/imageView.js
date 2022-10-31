30da0bc678afe07549a99e1c5873f4063e5aabae
yatsumehole <yatsumehole@naver.com>
2022-10-31 11:14:13 +0000

    drawRectOnChar 함수 코드 작성

--- js/ui/imageView.js
+++ js/ui/imageView.js
@@ -3,10 +3,14 @@ class ImageView {
 
   static updateImageView(info) {
     const imageCanvas = $("#image-canvas");
+    const rectCanvas = $("#rect-canvas");
     imageCanvas.width = info.size[0];
     imageCanvas.height = info.size[1];
     imageCanvas.style.width = info.size[0] + "px";
     imageCanvas.style.height = info.size[1] + "px";
+
+    rectCanvas.width = info.size[0];
+    rectCanvas.height = info.size[1];
 
     const scaleFactor = 300 / 96; // 300dpi로 설정
     imageCanvas.width = Math.ceil(imageCanvas.width * scaleFactor);
@@ -20,9 +24,27 @@ class ImageView {
 
   static drawRectOnChar(td) {
     const tr = td.parentElement;
-    const lineIdx = tr.dataset.trIdx;
-    const charIdx = td.dataset.tdIdx;
+    const lineIdx = parseInt(tr.dataset.trIdx);
+    const charIdx = parseInt(td.dataset.tdIdx);
     // 나중에 페이지 위치도 알 수 있게 추가하기
     const pageIdx = 1;
+
+    const cropPoints = Database.currentProject.info[0].cropPoints;
+    const cropPointH = cropPoints.horizontal;
+    const cropPointV = cropPoints.vertical;
+
+    const rectCanvas = $("#rect-canvas");
+    const canvasWidth = rectCanvas.width;
+    const canvasHeight = rectCanvas.height;
+    const ctx = rectCanvas.getContext("2d");
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.strokeStyle = "#ff0000";
+    ctx.strokeWidth = 2;
+
+    const x = cropPointH[charIdx];
+    const y = cropPointV[lineIdx];
+    const w = cropPointH[charIdx + 1] - cropPointH[charIdx];
+    const h = cropPointV[lineIdx + 1] - cropPointV[lineIdx];
+    ctx.strokeRect(x, y, w, h);
   }
 }
