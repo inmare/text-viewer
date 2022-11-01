b4bbe93366a6fb5747f46c78f0e24508fa828064
yatsumehole <yatsumehole@naver.com>
2022-11-01 11:57:03 +0000

    header 버튼 추가 및 이벤트 리스너 할당

--- js/ui/header.js
+++ js/ui/header.js
@@ -2,18 +2,28 @@ class Header {
   static initialize() {
     // header의 버튼들에 eventListener할당
     const changeViewBtn = $("#change-view");
+    const charSettingBtn = $("#toggle-char-setting");
+    const loadSettingBtn = $("#toggle-load-setting");
     // change-view 버튼 데이터 속성 초기화
     this.changeViewBtnData(changeViewBtn);
 
     changeViewBtn.addEventListener("click", TextView.changeView);
+    charSettingBtn.addEventListener("click", Control.toggleCharSetting);
+    loadSettingBtn.addEventListener("click", Control.toggleLoadInfo);
   }
 
   static changeViewBtnData(btn) {
-    const logoClassList = btn.querySelector("i").classList;
-    if (logoClassList.contains("header-btn-deactivate")) {
+    const logoClassList = btn.classList;
+    if (logoClassList.contains("header-btn-deactive")) {
       btn.setAttribute("data-view-mode", "from");
     } else {
       btn.setAttribute("data-view-mode", "to");
     }
   }
+
+  static toggleDeactiveCls(btn) {
+    btn.classList.toggle("header-btn-deactive");
+  }
+
+  static toggleImageZoomCls() {}
 }
