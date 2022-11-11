d878edcf61b0c246b815f4662b2ac47e60a13737
yatsumehole <yatsumehole@naver.com>
2022-11-11 03:26:53 +0000

    인수에 아무것도 없을 때 행동 추가

--- js/ui/positionView.js
+++ js/ui/positionView.js
@@ -1,18 +1,30 @@
 class PositionView {
-  static showCurrentProj() {
+  static showCurrentProj(projectName) {
     const currentProject = $("span[data-current='project']");
-    currentProject.innerText = Database.currentProject.name;
+
+    if (projectName === undefined) {
+      currentProject.innerText = "";
+      return;
+    }
+
+    currentProject.innerText = projectName;
   }
 
   static showCurrentPage(pageIdx) {
-    const currentProject = $("span[data-current='page']");
-    currentProject.innerText = Number(pageIdx) + 1;
+    const currentPage = $("span[data-current='page']");
+
+    if (pageIdx === undefined) {
+      currentPage.innerText = "";
+      return;
+    }
+
+    currentPage.innerText = Number(pageIdx) + 1;
   }
 
   static showCurrentTdPos(index) {
     const currentPos = $("span[data-current='pos']");
 
-    if (!index) {
+    if (index === undefined) {
       currentPos.innerText = "";
       return;
     }
