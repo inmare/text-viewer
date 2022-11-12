fb42c43509babbc973c3386ca8f3a4be9d5ecc25
yatsumehole <yatsumehole@naver.com>
2022-11-12 09:28:48 +0000

    savepage 함수 추가

--- js/database/changeData.js
+++ js/database/changeData.js
@@ -23,4 +23,44 @@ class ChangeData {
       await Database.deleteProjectFromId(id);
     }
   }
+
+  static savePage() {
+    const textTable = $("#text-table");
+    let pageText = textTable.textContent;
+
+    const viewModeBtn = $("#view-mode");
+    const mode = viewModeBtn.dataset.viewMode;
+    const charList = CharTable.charList;
+
+    switch (mode) {
+      case "from":
+        pageText = changeLinebreakAndTab();
+        break;
+      case "to":
+        for (let td of textViewTd) {
+          if (charList.from.includes(td.innerText)) {
+            const idx = charList.from.indexOf(td.innerText);
+            td.innerText = charList.to[idx];
+          }
+        }
+        break;
+    }
+
+    function changeLinebreakAndTab() {
+      let text = pageText;
+      if (charList.from.includes("\\n")) {
+        const linebreakIdx = charList.from.indexOf("\\n");
+        const linebreakTo = charList.to[linebreakIdx];
+        text = pageText.replace(linebreakTo, "\n");
+      }
+
+      if (charList.from.includes("\\t")) {
+        const linebreakIdx = charList.from.indexOf("\\t");
+        const linebreakTo = charList.to[linebreakIdx];
+        text = pageText.replace(linebreakTo, "\t");
+      }
+
+      return text;
+    }
+  }
 }
