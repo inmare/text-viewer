accb3adc678cf89a3a1ed40eddefffa322d80d83
yatsumehole <yatsumehole@naver.com>
2022-11-12 05:48:29 +0000

    text-table에 보여지는 글자들에 대한 함수 이름 변경 및 코드 수정

--- js/ui/header.js
+++ js/ui/header.js
@@ -1,29 +1,40 @@
 class Header {
   static initialize() {
     // header의 버튼들에 eventListener할당
-    const changeViewBtn = $("#change-view");
+    const viewModeBtn = $("#view-mode");
     const charSettingBtn = $("#toggle-char-setting");
     const loadSettingBtn = $("#toggle-load-setting");
+    const savePageBtn = $("#save-page");
+    const initPageBtn = $("#init-page");
     // change-view 버튼 데이터 속성 초기화
-    this.changeViewBtnData(changeViewBtn);
+    Header.toggleViewMode();
 
-    changeViewBtn.addEventListener("click", TextView.changeView);
+    viewModeBtn.addEventListener("click", Header.toggleViewMode);
     charSettingBtn.addEventListener("click", Control.toggleCharSetting);
     loadSettingBtn.addEventListener("click", Control.toggleLoadInfo);
+    savePageBtn.addEventListener("click", ChangeData.savePage);
   }
 
-  static changeViewBtnData(btn) {
+  static toggleViewMode() {
+    const btn = $("#view-mode");
+    Header.toggleDeactiveCls(btn);
+
     const logoClassList = btn.classList;
     if (logoClassList.contains("header-btn-deactive")) {
       btn.setAttribute("data-view-mode", "from");
     } else {
       btn.setAttribute("data-view-mode", "to");
     }
+
+    TextView.changeTextTableMode();
   }
 
   static toggleDeactiveCls(btn) {
     btn.classList.toggle("header-btn-deactive");
   }
 
-  static toggleImageZoomCls() {}
+  static toggleImageZoomCls(btn) {
+    btn.classList.toggle("fa-magnifying-glass-plus");
+    btn.classList.toggle("fa-magnifying-glass-minus");
+  }
 }
