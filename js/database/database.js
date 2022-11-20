001fb8bd9393addca0fcdb3474d466ab4ce626ff
yatsumehole <yatsumehole@naver.com>
2022-11-20 05:54:59 +0000

    새로운 object store 생성

--- js/database/database.js
+++ js/database/database.js
@@ -2,15 +2,16 @@ class Database {
   static currentProject;
 
   static async initialize() {
-    const imgData = await getImage();
-    const textData = await getText();
-
     const dbOpenRequest = indexedDB.open(DATABASE_NAME, 1);
 
     // 처음 데이터 베이스를 생성해서 버전이 0일때
     dbOpenRequest.onupgradeneeded = function () {
       const db = dbOpenRequest.result;
-      db.createObjectStore(OBJECT_STORE_NAME, {
+      db.createObjectStore(DATA_STORE_NAME, {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      db.createObjectStore(IMAGE_STORE_NAME, {
         keyPath: "id",
         autoIncrement: true,
       });
@@ -22,79 +23,7 @@ class Database {
     };
 
     dbOpenRequest.onsuccess = async function () {
-      // 임시 데이터 추가하기
       console.log("데이터 베이스 불러오기 성공");
-      const db = dbOpenRequest.result;
-      const transaction = db.transaction(OBJECT_STORE_NAME, "readwrite");
-      const projects = transaction.objectStore(OBJECT_STORE_NAME);
-
-      // 샘플 데이터 만들기
-      const proj = {
-        id: "SAMPLE_PROJ", // 유일해야 하는 값. 나중에 랜덤한 해시 붙이기
-        name: "PROJECT_NAME",
-        lastSaved: new Date(),
-        charPerLine: 20,
-        linePerPage: 10,
-        info: [
-          {
-            data: imgData,
-            size: [200, 200], // [x, y]
-            text: textData,
-            changedText: null,
-            cropPoints: {
-              horizontal: [
-                0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140,
-                150, 160, 170, 180, 190, 200,
-              ],
-              vertical: [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200],
-            },
-            lowPercentChar: [
-              {
-                char: "L",
-                index: 0,
-              },
-              {
-                char: "o",
-                index: 1,
-              },
-            ],
-          },
-          {
-            data: imgData,
-            size: [200, 200], // [x, y]
-            text: textData,
-            changedText: null,
-            cropPoints: {
-              horizontal: [
-                0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140,
-                150, 160, 170, 180, 190, 200,
-              ],
-              vertical: [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200],
-            },
-            lowPercentChar: [
-              {
-                char: "r",
-                index: 2,
-              },
-              {
-                char: "e",
-                index: 3,
-              },
-            ],
-          },
-        ],
-      };
-
-      // 이미 존재하는 id라면 덮어쓰기를 하기위해 put으로 넣음
-      const request = projects.put(proj);
-
-      request.onsuccess = function () {
-        console.log("데이터 입력 성공");
-      };
-
-      request.onerror = function () {
-        console.log("Error", request.error);
-      };
     };
   }
 
