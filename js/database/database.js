5a4cdabcb974601092a546c5070919d8aa2ba53d
yatsumehole <yatsumehole@naver.com>
2022-11-20 06:42:10 +0000

    데이터베이스 이름 변경 및 saveImage 함수 추가

--- js/database/database.js
+++ js/database/database.js
@@ -39,8 +39,8 @@ class Database {
         // 임시 데이터 추가하기
         console.log("데이터 베이스 불러오기 성공");
         const db = dbOpenRequest.result;
-        const transaction = db.transaction(OBJECT_STORE_NAME, "readonly");
-        const projects = transaction.objectStore(OBJECT_STORE_NAME);
+        const transaction = db.transaction(DATA_STORE_NAME, "readonly");
+        const projects = transaction.objectStore(DATA_STORE_NAME);
 
         // 프로젝트의 갯수가 적을 거라고 가정하고 getAll사용
         // 나중에는 cursor를 사용하는 것도 고려해보기
@@ -69,8 +69,8 @@ class Database {
         // 임시 데이터 추가하기
         console.log("데이터 베이스 불러오기 성공");
         const db = dbOpenRequest.result;
-        const transaction = db.transaction(OBJECT_STORE_NAME, "readonly");
-        const projects = transaction.objectStore(OBJECT_STORE_NAME);
+        const transaction = db.transaction(DATA_STORE_NAME, "readonly");
+        const projects = transaction.objectStore(DATA_STORE_NAME);
 
         const request = projects.get(id);
 
@@ -97,8 +97,8 @@ class Database {
         // 임시 데이터 추가하기
         console.log("데이터 베이스 불러오기 성공");
         const db = dbOpenRequest.result;
-        const transaction = db.transaction(OBJECT_STORE_NAME, "readwrite");
-        const projects = transaction.objectStore(OBJECT_STORE_NAME);
+        const transaction = db.transaction(DATA_STORE_NAME, "readwrite");
+        const projects = transaction.objectStore(DATA_STORE_NAME);
 
         projects.delete(id);
       };
@@ -116,10 +116,37 @@ class Database {
       // 임시 데이터 추가하기
       console.log("데이터 베이스 불러오기 성공");
       const db = dbOpenRequest.result;
-      const transaction = db.transaction(OBJECT_STORE_NAME, "readwrite");
-      const projects = transaction.objectStore(OBJECT_STORE_NAME);
+      const transaction = db.transaction(DATA_STORE_NAME, "readwrite");
+      const projects = transaction.objectStore(DATA_STORE_NAME);
 
       const request = projects.put(Database.currentProject);
+
+      request.onsuccess = function () {
+        console.log("데이터 저장 성공");
+      };
+
+      request.onerror = function () {
+        console.log("Error", request.error);
+        throw request.error;
+      };
+    };
+  }
+
+  static saveImage(imageObj) {
+    const dbOpenRequest = indexedDB.open(DATABASE_NAME, 1);
+
+    dbOpenRequest.onerror = function () {
+      console.error("Error", dbOpenRequest.error);
+    };
+
+    dbOpenRequest.onsuccess = function () {
+      // 임시 데이터 추가하기
+      console.log("데이터 베이스 불러오기 성공");
+      const db = dbOpenRequest.result;
+      const transaction = db.transaction(IMAGE_STORE_NAME, "readwrite");
+      const projects = transaction.objectStore(IMAGE_STORE_NAME);
+
+      const request = projects.put(imageObj);
 
       request.onsuccess = function () {
         console.log("데이터 저장 성공");
