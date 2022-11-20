3bc998d0ebdf4855f99de98cb022251e6694e799
yatsumehole <yatsumehole@naver.com>
2022-11-20 10:50:58 +0000

    json파일 및 이미지 로드 함수 추가

--- js/temp.js
+++ js/temp.js
@@ -1,23 +1,11 @@
-async function getText() {
-  const res = await fetch("./lorem-short.txt");
-  const text = await res.text();
-  return text;
-}
-
 async function getImage() {
-  return new Promise((resolve, reject) => {
+  /*
     const img = new Image();
-    img.src = "./lorem-short.png";
+    img.src = "./sample/Sample.png";
     img.onload = function () {
       const canvas = document.createElement("canvas");
       canvas.width = img.width;
       canvas.height = img.height;
-      // canvas.style.width = img.width + "px";
-      // canvas.style.height = img.height + "px";
-
-      // const scaleFactor = 300 / 96; // 300dpi로 설정
-      // canvas.width = Math.ceil(canvas.width * scaleFactor);
-      // canvas.height = Math.ceil(canvas.height * scaleFactor);
 
       const ctx = canvas.getContext("2d");
       ctx.globalCompositeOperation = "luminosity";
@@ -27,5 +15,62 @@ async function getImage() {
       const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
       resolve(imgData);
     };
-  });
+    */
+  const res = await fetch("./sample/Sample.png");
+  const blob = await res.blob();
+
+  return blob;
+}
+
+async function getJSON() {
+  const jsonFile = await fetch("./sample/RANDOM_PROJECT_0494.json");
+  const result = await jsonFile.json();
+
+  return result;
+}
+
+async function makePlaceholder() {
+  // json 파일 가져오기
+  const jsonData = await getJSON();
+  // 가져온 파일에 마지막 저장 날짜 추가하기
+  jsonData.lastSaved = new Date();
+  // json파일 데이터 저장
+  Database.currentProject = jsonData;
+  Database.savePage();
+  // 이미지 파일 가져오기
+  const sampleImgBlob = await getImage();
+  // const imgData = sampleImgData.data;
+
+  for (let i = 0; i < 2; i++) {
+    setTimeout(async () => {
+      // const imageArray = reduceArraySize(imgData);
+      await saveImageToDB(sampleImgBlob, i);
+    });
+  }
+
+  function reduceArraySize(imgData) {
+    let imageArray = [];
+    for (let i = 0; i < imgData.length; i += 4) {
+      const value = Math.round(
+        imgData[i] * 0.2125 + imgData[i + 1] * 0.7154 + imgData[i + 2] * 0.0721
+      );
+      imageArray.push(value);
+    }
+    return imageArray;
+  }
+
+  // 이미지 파일 데이터 저장
+  async function saveImageToDB(imageArray, pageIdx) {
+    const imageObj = {
+      // id: jsonData.id + String(pageIdx + 1).padStart(2, "0"),
+      projectId: jsonData.id,
+      page: pageIdx,
+      data: imageArray,
+    };
+
+    Database.saveImage(imageObj);
+  }
+
+  // 이미지 파일을 1d array로 만들기
+  // 이미지 파일 데이터에 array, 프로젝트 이름, 페이지 추가
 }
