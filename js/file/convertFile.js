848a3551acc1e8113e1e56c148fd06a129fe7f56
yatsumehole <yatsumehole@naver.com>
2022-11-20 13:57:29 +0000

    파일을 읽고 데이터베이스에 저장하는 함수 작성

--- js/file/convertFile.js
+++ js/file/convertFile.js
@@ -1,8 +1,56 @@
 class ConvertFile {
-  static fileToData(e) {
+  static async fileToData(e) {
     const files = e.target.files;
+    const jsonRegex = /\.json$/;
+    const imageRegex = /\.(png|jpg|jpeg)$/;
+    let jsonData;
+    let imageFileList = [];
     for (let file of files) {
-      console.log(file.name);
+      if (file.name.match(jsonRegex)) {
+        jsonData = await ConvertFile.fileToJSON(file);
+      } else if (file.name.match(imageRegex)) {
+        imageFileList.push(file);
+      }
     }
+
+    imageFileList.sort((a, b) => {
+      if (a.name > b.name) return 1;
+      else if (a.name == b.name) return 0;
+      else return -1;
+    });
+
+    if (imageFileList.length != jsonData.imageInfo.length) {
+      return alert(
+        "JSON파일의 이미지 개수와 실제 이미지 파일 개수가 다릅니다."
+      );
+    }
+
+    jsonData.lastSaved = new Date();
+    Database.currentProject = jsonData;
+    Database.saveProject();
+
+    console.log(imageFileList);
+
+    for (let [fileIdx, file] of imageFileList.entries()) {
+      console.log(imageFileList.indexOf(file));
+      const imageObj = {
+        data: file,
+        page: fileIdx,
+        projectId: Database.currentProject.id,
+      };
+
+      Database.saveImage(imageObj);
+    }
+  }
+
+  static async fileToJSON(file) {
+    return new Promise((resolve, _) => {
+      const fileReader = new FileReader();
+      fileReader.readAsText(file);
+      fileReader.onload = function () {
+        const json = JSON.parse(fileReader.result);
+        resolve(json);
+      };
+    });
   }
 }
